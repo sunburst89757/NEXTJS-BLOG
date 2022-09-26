46c4550d41988e5499a8c6d8f688d4b9d72a25ae
sunburst89757 <3520279278@qq.com>
2022-09-26 14:16:57 +0800

    add blog dynamic routes

--- pages/index.jsx
+++ pages/index.jsx
@@ -1,5 +1,6 @@
 import Head from "next/head";
 import Link from "next/link";
+import Date from "../components/date";
 import Layout, { siteTitle } from "../components/layout";
 import { getSortedPostsData } from "../lib/post";
 import utilStyles from "../styles/utils.module.css";
@@ -23,11 +24,13 @@ export default function Home({ allPostsData }) {
         <ul className={utilStyles.list}>
           {allPostsData.map(({ id, date, title }) => (
             <li className={utilStyles.listItem} key={id}>
-              {title}
+              <Link href={`/posts/${id}`}>
+                <a>{title}</a>
+              </Link>
               <br />
-              {id}
-              <br />
-              {date}
+              <small className={utilStyles.lightText}>
+                <Date dateString={date} />
+              </small>
             </li>
           ))}
         </ul>
---
+++ pages/posts/[id].jsx
@@ -0,0 +1,40 @@
+import Head from "next/head";
+import Date from "../../components/date";
+import Layout from "../../components/layout";
+import { getAllPostIds, getPostData } from "../../lib/post";
+import utilStyles from "../../styles/utils.module.css";
+
+export default function Post({ postData }) {
+  return (
+    <Layout>
+      <Head>
+        <title>{postData.title}</title>
+      </Head>
+      <article>
+        <h1 className={utilStyles.headingXl}>{postData.title}</h1>
+        <div className={utilStyles.lightText}>
+          <Date dateString={postData.date} />
+        </div>
+        <div dangerouslySetInnerHTML={{ __html: postData.contentHtml }} />
+      </article>
+    </Layout>
+  );
+}
+//  用于区分路由组件不同的路由id
+export async function getStaticPaths() {
+  // Return a list of possible value for id
+  const paths = getAllPostIds();
+  return {
+    paths,
+    fallback: false,
+  };
+}
+//  根据id获取路由组件需要的值
+export async function getStaticProps({ params }) {
+  const postData = await getPostData(params.id);
+  return {
+    props: {
+      postData,
+    },
+  };
+}
